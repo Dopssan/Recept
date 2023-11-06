8a29e5d87b96c3b991149d07fe846ea5684976d3
Dopssan <150046622+Dopssan@users.noreply.github.com>
2023-11-06 16:12:32 +0200

    Add files via upload
    laba2

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+	<title>HelperCook</title>
+
+</head>
+<body>
+
+<header data-bs-theme="dark">
+  <div class="collapse text-bg-dark" id="navbarHeader">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 col-md-7 py-4">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container">
+      <a class="navbar-brand d-flex align-items-center">
+        <strong>Кулінарний допомічник</strong>
+      </a>
+      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Контакти</a>
+        <a class="me-3 py-2 link-body-emphasis text-decoration-none" href="#">Рецепти</a>
+      </nav>
+        
+    </div>
+  </div>
+  <div class="container mt-4">
+  <h3 style="font-size: 43px;"> Страви </h3>
+</div>
+
+<div class="container mt-5"
+
+</header>
+
+<?php
+	for($i=0;$i<2;$i++);
+?>
+
+<div class="col">
+          <div class="card shadow-sm align-items-center">
+          	<h3> Борщ </h3>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: " preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect></svg>
+            <div class="card-body">
+              <p class="card-text">Борщ - це традиційна українська страва, яка є популярною не лише в Україні, але й в багатьох інших країнах Східної та Центральної Європи. Ця страва має багато варіацій, і кожен регіон може мати свій власний спосіб готування борщу.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group ">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Дивитися</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Інгрідієнти</button>
+                </div>
+                <small class="text-body-secondary">Час приготування 45 хвилин</small>
+              </div>
+            </div>
+          </div>
+        </div>
+</div>
+
+
+
+</body>
+</html>
